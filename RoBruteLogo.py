ec4de260dc0cf5d4fd499a28566aeecc6a5fa48c
RussianOtter <elangs321@gmail.com>
2017-02-05 16:50:43 -0500

    Version 4

---
+++ RoBruteLogo.py
@@ -0,0 +1,18 @@
+import sys, console, time
+# copyright SavSec
+print "         ___           ___     "
+print "        /\  \         /\  \    "
+sys.stdout.write("       /::\  \  ")
+console.set_color(0,1,0)
+sys.stdout.write("v5")
+console.set_color()
+sys.stdout.write("   /::\  \   \n")
+print "      /:/\:\  \     /:/\:\  \  "
+print "     /::\~\:\  \   /:/  \:\  \ "
+print "    /:/\:\ \:\__\ /:/__/ \:\__\ "
+print "    \/_|::\/:/  / \:\  \ /:/  / "
+print "      B|:|::/  /R U\:\T /:/  /E"
+print "       |:|\/__/     \:\/:/  /  "
+print "       |:|  |        \::/  /   "
+print "        \|__|         \/__/    "
+print ""
